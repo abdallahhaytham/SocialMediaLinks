801461edfdfa43743c2b09efe47c4123ed769225
Abdal <Abdal@DELL-LATITUDE-3>
2025-08-28 07:47:07 +0300

    Add project files.

---
+++ SocialMediaLinks/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SocialMediaLinks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _socialMediaLinksOptions;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IOptions<SocialMediaLinksOptions> options,IWebHostEnvironment webHostEnvironment) 
+        {
+
+            _socialMediaLinksOptions = options.Value;
+
+
+
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.EnvironmentName = _webHostEnvironment.EnvironmentName;
+
+            SocialMediaLinksOptions socialMediaLinksOptions = new SocialMediaLinksOptions()
+            {
+
+
+               Facebook = _socialMediaLinksOptions.Facebook,
+                Twitter = _socialMediaLinksOptions.Twitter,
+
+                Youtube = _socialMediaLinksOptions.Youtube,
+
+                Instagram = _socialMediaLinksOptions.Instagram
+
+
+            };
+
+            ViewBag.SocialMediaLinks = socialMediaLinksOptions;
+            return View();
+        }
+    }
+}
